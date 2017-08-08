efe4b743d610505c41cb3d58580b7a6458157085
Zymon P. Castaneda <zymon.cas@leekie.com>
2017-08-08 12:51:49 +0800

    This is the first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Izuku Midoriya' },
+  { id: 12, name: 'Katsuki Bakugou' },
+  { id: 13, name: 'Shouto Todoroki' },
+  { id: 14, name: 'Ochako Uraraka' },
+  { id: 15, name: 'Tsuyu Asui' },
+  { id: 16, name: 'Tenya Iida' },
+  { id: 17, name: 'Momo Yaoyorozu' },
+  { id: 18, name: 'Fumikage Tokoyami' },
+  { id: 19, name: 'Toshinori Yagi' },
+  { id: 20, name: 'Shota Aizawa' }
+];
